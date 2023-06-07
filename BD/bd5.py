6cb13621fb592966fa2c7a9f2b4797e47ab0eda3
Xavier <xavier.sastre@cide.es>
2023-06-07 11:59:45 +0200

    Afegit Notes.db

--- BD/bd5.py
+++ BD/bd5.py
@@ -2,11 +2,16 @@ from AlumneBDminified import *
 nouAlumne=Alumne('Llorenç','Capellà','Vich',34)
 print(nouAlumne.toString()+" creat satisfactòriament.")
 idnouAlumne=nouAlumne.guardaraBD()
+nouAlumne2=Alumne('Tofol','Verdera','Damià',23)
+idnouAlumne2=nouAlumne2.guardaraBD()
 print(nouAlumne.toString()+" guardat a la BD correctament.")
 print(nouAlumne.mostrarAlumneBD(idnouAlumne))
+print(nouAlumne2.mostrarAlumneBD(idnouAlumne2))
 if (nouAlumne.eliminarAlumneBD()):
     print(nouAlumne.toString()+" eliminat de la BD correctament")
 else:
     print(nouAlumne.toString()+" no s'ha eliminat de la BD")
 anticAlumne=Alumne(2)
+print(nouAlumne.nom)
+print(nouAlumne.mostrarAlumneBD(22))
 print(anticAlumne.nom)
